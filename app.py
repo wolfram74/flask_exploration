99280e598fe40a503d99a8e25af7b6403fd94e4d
wolfram74 <phaugen@gmail.com>
2016-04-16 19:18:57 -0500

    error handling

--- app.py
+++ app.py
@@ -19,9 +19,13 @@ def login_required(f):
 @app.route('/')
 @login_required
 def home():
-    g.db = connect_db()
-    cur = g.db.execute('select * from posts')
-    posts= [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
+    try:
+        g.db = connect_db()
+        cur = g.db.execute('select * from posts')
+        posts = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
+    except:
+        flash('database error')
+        posts=[]
     return render_template('index.html', posts=posts)
 
 @app.route('/welcome')

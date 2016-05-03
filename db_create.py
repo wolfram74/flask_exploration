388f339ec33cd1ceecb5c771468cc4045e2423fd
wolfram74 <phaugen@gmail.com>
2016-05-03 13:40:25 -0500

    making stuffs

--- db_create.py
+++ db_create.py
@@ -7,4 +7,7 @@ db.create_all()
 db.session.add(BlogPost('Good', "I\'m good"))
 db.session.add(BlogPost('Well', "I\'m well"))
 
+db.session.add(User('admin', 'ad@min.com', 'admin'))
+
 db.session.commit()
+

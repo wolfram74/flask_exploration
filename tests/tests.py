6b8081849145f501d1468703dc05928c184441d6
wolfram74 <phaugen@gmail.com>
2016-05-17 17:19:05 -0500

    added more testing framework

--- tests/tests.py
+++ tests/tests.py
@@ -3,25 +3,8 @@ from flask.ext.testing import TestCase
 from project import app, db
 from project.models import User, BlogPost
 from flask.ext.login import current_user
+from base import BaseTestCase
 
-class BaseTestCase(TestCase):
-    def create_app(self):
-        app.config.from_object('config.TestConfig')
-        return app
-
-    def setUp(self):
-        # self.tester = app.test_client(self)
-        self.good_cred = dict(username='admin', password='admin')
-        self.bad_cred = dict(username='buttts', password='farts')
-        db.create_all()
-        db.session.add(BlogPost('Test post', 'This is a test. Only a test.', 1))
-        db.session.add(User('admin', 'ad@min.com', 'admin'))
-        db.session.commit()
-
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
-        pass
 
 class FlaskTestCase(BaseTestCase):
 
--- tests/tests_auth.py
+++ tests/tests_auth.py
@@ -2,26 +2,7 @@ import unittest
 from flask.ext.testing import TestCase
 from project import app, db
 from project.models import User, BlogPost
-
-class BaseTestCase(TestCase):
-    def create_app(self):
-        app.config.from_object('config.TestConfig')
-        return app
-
-    def setUp(self):
-        # self.tester = app.test_client(self)
-        self.good_cred = dict(username='admin', password='admin')
-        self.bad_cred = dict(username='buttts', password='farts')
-        db.create_all()
-        db.session.add(BlogPost('Test post', 'This is a test. Only a test.', 1))
-        db.session.add(User('admin', 'ad@min.com', 'admin'))
-        db.session.commit()
-
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
-        pass
-
+from base import BaseTestCase
 
 class FlaskTestCase(BaseTestCase):
 
